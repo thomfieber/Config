b55a3d96972cd60a4f8ae8834b21185d95a79ae0
Thomas <45907694+thomfieber@users.noreply.github.com>
2020-11-21 06:06:56 -0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,8 +4,6 @@ cp .zshrc ~/ && cp .tmux.conf ~/ && sudo cp mount-shared-folders /usr/local/sbin
 
 ln -s /mnt/hgfs/VM_Share ~/share
 
-sudo mv vpn*.sh /opt
-
 sudo apt update && sudo apt full-upgrade 
 
 sudo apt install -y --reinstall open-vm-tools-desktop fuse xclip seclists payloadsallthethings kerberoast python-is-python3 masscan dsniff dnsrecon dnsenum busybox html2text edb-debugger sshuttle socat beef-xss powershell-empire joomscan jboss-autopwn mingw-w64 pure-ftpd atftp wine xclip exiftool rinetd tor python3-pip
