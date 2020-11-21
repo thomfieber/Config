9d39444384d279280c7db77a69a7e688ade5e457
Thomas <45907694+thomfieber@users.noreply.github.com>
2020-11-20 20:32:30 -0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-sudo cp vpn*.sh /opt && cp .zshrc ~/ && cp .tmux.conf ~/ && sudo cp mount-shared-folders /usr/local/sbin && sudo chmod +x /usr/local/sbin/mount-shared-folders && sudo /usr/local/sbin/mount-shared-folders && echo '@reboot sudo /usr/local/sbin/mount-shared-folders' >> /etc/crontab
+cp .zshrc ~/ && cp .tmux.conf ~/ && sudo cp mount-shared-folders /usr/local/sbin && sudo chmod +x /usr/local/sbin/mount-shared-folders && sudo /usr/local/sbin/mount-shared-folders && sudo cp vpn*.sh /opt/
 
-sudo apt update && sudo apt install -y --reinstall open-vm-tools-desktop fuse xclip 
+sudo apt update && sudo apt full-upgrade 
+
+sudo apt install -y --reinstall open-vm-tools-desktop fuse xclip seclists payloadsallthethings kerberoast python-is-python3 masscan dsniff dnsrecon dnsenum busybox html2text edb-debugger sshuttle socat beef-xss powershell-empire joomscan jboss-autopwn mingw-w64 pure-ftpd atftp wine xclip exiftool rinetd tor
+
+chsh -s $(which zsh)
+
