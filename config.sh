5bcd93cd279e76f81dbfc7425e9f7e45a9056582
Thomas <45907694+thomfieber@users.noreply.github.com>
2020-11-20 20:34:55 -0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,7 +4,9 @@ cp .zshrc ~/ && cp .tmux.conf ~/ && sudo cp mount-shared-folders /usr/local/sbin
 
 sudo apt update && sudo apt full-upgrade 
 
-sudo apt install -y --reinstall open-vm-tools-desktop fuse xclip seclists payloadsallthethings kerberoast python-is-python3 masscan dsniff dnsrecon dnsenum busybox html2text edb-debugger sshuttle socat beef-xss powershell-empire joomscan jboss-autopwn mingw-w64 pure-ftpd atftp wine xclip exiftool rinetd tor
+sudo apt install -y --reinstall open-vm-tools-desktop fuse xclip seclists payloadsallthethings kerberoast python-is-python3 masscan dsniff dnsrecon dnsenum busybox html2text edb-debugger sshuttle socat beef-xss powershell-empire joomscan jboss-autopwn mingw-w64 pure-ftpd atftp wine xclip exiftool rinetd tor python3-pip
+
+sudo gem install pry evil-winrm
 
 chsh -s $(which zsh)
 
