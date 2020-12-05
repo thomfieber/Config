acf63d67e7d2374c673adf1c08d99bfeb33b0633
thomfieber <thomfieber@gmail.com>
2020-12-04 19:03:12 -0600

    Update utility downloads

--- config.sh
+++ config.sh
@@ -2,13 +2,21 @@
 
 cp .zshrc ~/ && cp .tmux.conf ~/ && sudo cp mount-shared-folders /usr/local/sbin && sudo chmod +x /usr/local/sbin/mount-shared-folders && sudo /usr/local/sbin/mount-shared-folders
 
+# Mount shared folders
 ln -s /mnt/hgfs/VM_Share ~/share
 
-sudo apt update && sudo apt full-upgrade 
+# Update the system
+sudo apt update && sudo apt full-upgrade
 
-sudo apt install -y --reinstall open-vm-tools-desktop fuse xclip seclists payloadsallthethings kerberoast python-is-python3 masscan dsniff dnsrecon dnsenum busybox html2text edb-debugger sshuttle socat beef-xss powershell-empire joomscan jboss-autopwn mingw-w64 pure-ftpd atftp wine xclip exiftool rinetd tor python3-pip
+# Install utilities from the repo
+sudo apt install -y --reinstall open-vm-tools-desktop fuse xclip seclists payloadsallthethings kerberoast python-is-python3 masscan dsniff dnsrecon dnsenum busybox html2text edb-debugger sshuttle socat beef-xss powershell-empire joomscan jboss-autopwn mingw-w64 pure-ftpd atftp wine xclip exiftool rinetd tor python3-pip telnet golang dotdotpwn steghide gcc-multilib bloodhound gvm rlwrap gobuster sublist3r foremost
 
+# Install ruby utilities
 sudo gem install pry evil-winrm
 
-chsh -s $(which zsh)
+# Install python utilities with pip3
+sudo pip3 install stegoveritas
 
+# Move into share and install utilities
+cd /opt
+for dir in $(cat ~/share/utilities.txt); do sudo git clone $dir; done
