6914da0c85aaee0a23d893f47bf539ccc0362717
Thomas <45907694+thomfieber@users.noreply.github.com>
2020-12-17 04:24:10 -0600

    Update config.sh
    Added sudo

--- config.sh
+++ config.sh
@@ -11,7 +11,7 @@ sudo apt update && sudo apt full-upgrade
 # {fortisslvpn,l2tp,openconnect,openvpn,pptp,ssh,strongswan,vpnc}
 
 # Install utilities from the repo
-apt install -y --reinstall open-vm-tools-desktop fuse xclip seclists payloadsallthethings kerberoast python-is-python3 masscan dsniff dnsrecon dnsenum busybox html2text edb-debugger sshuttle socat beef-xss powershell-empire joomscan jboss-autopwn mingw-w64 pure-ftpd atftp wine xclip exiftool rinetd tor python3-pip telnet golang dotdotpwn steghide gcc-multilib bloodhound gvm rlwrap gobuster sublist3r foremost network-manager-*-gnome virtualenv diodon gtk2-engines-murrine gtk2-engines-pixbuf remmina powercat shellter wine crowbar tor httptunnel
+sudo apt install -y --reinstall open-vm-tools-desktop fuse xclip seclists payloadsallthethings kerberoast python-is-python3 masscan dsniff dnsrecon dnsenum busybox html2text edb-debugger sshuttle socat beef-xss powershell-empire joomscan jboss-autopwn mingw-w64 pure-ftpd atftp wine xclip exiftool rinetd tor python3-pip telnet golang dotdotpwn steghide gcc-multilib bloodhound gvm rlwrap gobuster sublist3r foremost network-manager-*-gnome virtualenv diodon gtk2-engines-murrine gtk2-engines-pixbuf remmina powercat shellter wine crowbar tor httptunnel
 
 # Install ruby utilities
 sudo gem install pry evil-winrm
@@ -21,4 +21,4 @@ sudo pip3 install stegoveritas
 
 # Move into share and install utilities
 cd /opt
-for dir in $(cat ~/share/utilities.txt); do sudo git clone $dir; done
+for dir in $(cat ~/share/Utilities/utilities.txt); do sudo git clone $dir; done
